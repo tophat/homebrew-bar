04003934881d387d50907344bf2b0da676604ef3
Emmanuel Ogbizi <iamogbz+github@gmail.com>
2019-04-09 22:44:49 -0400

    chore: update yvm formula (#14)
    * wip: scaffolding
    
    * chore: update yvm tap

--- Formula/yvm.rb
+++ Formula/yvm.rb
@@ -3,8 +3,8 @@ class Yvm < Formula
   desc "Manage multiple versions of Yarn"
   homepage "https://yvm.js.org"
   # Should only be updated if a newer version is listed as a stable release
-  url "https://github.com/tophat/yvm/releases/download/v2.4.1/yvm.zip"
-  sha256 "82e0e3ee1e66ad389641fef9d9c3a164124cd63e89323a77e03a30fa9d5ecfdf"
+  url "https://github.com/tophat/yvm/releases/download/v3.1.0/yvm.zip"
+  sha256 "c6131dc8d4be12706e66110e378da35978d1171e5b166904d827e3cf8fc678a8"
 
   bottle :unneeded
 
@@ -17,28 +17,23 @@ class Yvm < Formula
     update_self_disabled = "echo 'YVM update-self disabled. Use `brew upgrade yvm`.'"
     inreplace "yvm.sh" do |s|
       s.gsub! 'YVM_DIR=${YVM_DIR-"${HOME}/.yvm"}', "YVM_DIR='#{prefix}'"
-      s.gsub! "curl -fsSL https://raw.githubusercontent.com/tophat/yvm/master/scripts/install.sh | YVM_INSTALL_DIR=${YVM_DIR} bash", update_self_disabled
+      s.gsub! "curl -fsSL https://raw.githubusercontent.com/tophat/yvm/master/scripts/install.js | YVM_INSTALL_DIR=${YVM_DIR} node", update_self_disabled
     end
     inreplace "yvm.fish" do |s|
       s.gsub! 'set -q YVM_DIR; or set -U YVM_DIR "$HOME/.yvm"', "set -U YVM_DIR '#{prefix}'"
-      s.gsub! "env YVM_INSTALL_DIR=$YVM_DIR curl -fsSL https://raw.githubusercontent.com/tophat/yvm/master/scripts/install.sh | bash", update_self_disabled
+      s.gsub! "env YVM_INSTALL_DIR=$YVM_DIR curl -fsSL https://raw.githubusercontent.com/tophat/yvm/master/scripts/install.js | node", update_self_disabled
     end
     chmod 0755, "yvm.sh"
     makedirs "/usr/local/var/yvm/versions"
     ln_s "/usr/local/var/yvm/versions", "./versions"
     File.write(".version", "{ \"version\": \"#{version}\" }")
-    prefix.install [".version", "versions", "node_modules", "yvm.sh", "yvm.fish", "yvm.js", "yvm-exec.js"]
+    prefix.install [".version", "versions", "node_modules", "yvm.sh", "yvm.fish", "yvm.js"]
   end
 
   def caveats
     emptor = <<~EOS
-      To load yvm in the shell add to your ~/.bashrc or ~/.zsh
-      export YVM_DIR="#{prefix}"
-      [ -r $YVM_DIR/yvm.sh ] && source $YVM_DIR/yvm.sh
-
-      And for fishers, add to your ~/.config/fish/config.fish
-      set -x YVM_DIR "#{prefix}"
-      source . $YVM_DIR/yvm.fish
+      Run the following command to configure your shell rc file
+      $ node "#{prefix}/yvm.js" configure-shell
 
       If you have previously installed YVM, link the versions folder
       to allow all brewed YVM access to the managed yarn distributions
