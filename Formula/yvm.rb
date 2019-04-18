f57b8ef2f4fabe99da69ca3bbd339c796b7835a6
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-04-17 23:18:14 -0400

    wip: lnsf

--- Formula/yvm.rb
+++ Formula/yvm.rb
@@ -26,7 +26,7 @@ class Yvm < Formula
     chmod 0755, "yvm.sh"
     chmod 0755, "shim/yarn"
     mkdir_p "/usr/local/var/yvm/versions"
-    ln_s "/usr/local/var/yvm/versions", "./versions"
+    ln_sf "/usr/local/var/yvm/versions", "./versions"
     File.write(".version", "{ \"version\": \"#{version}\" }")
     prefix.install [".version", "versions", "node_modules", "shim", "yvm.sh", "yvm.fish", "yvm.js"]
   end
