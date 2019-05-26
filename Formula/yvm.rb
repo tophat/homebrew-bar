c09bf901a920b03dc194f502a89004537a1ba79c
Emmanuel Ogbizi <iamogbz+github@gmail.com>
2019-05-26 10:08:38 -0400

    chore: upgrade yvm formula (#19)
    * chore: upgrade yvm formula
    
    * cr: no shell system
    
    * cr: please the audit gods

--- Formula/yvm.rb
+++ Formula/yvm.rb
@@ -3,17 +3,22 @@ class Yvm < Formula
   desc "Manage multiple versions of Yarn"
   homepage "https://yvm.js.org"
   # Should only be updated if a newer version is listed as a stable release
-  url "https://github.com/tophat/yvm/releases/download/v3.3.0/yvm.zip"
-  sha256 "687376569e02c60cf2beeab25fe4966b341b22c82666dfbf55ddcb7a4cb13e11"
+  url "https://github.com/tophat/yvm/releases/download/v3.4.0/yvm.js"
+  sha256 "5fa5f4ae76063dde9ce6be016c610d610d2b72d59e6af4573dcc99c2e3bd9495"
 
   bottle :unneeded
 
-  depends_on "node" => [:recommended] # Can be ignored if node already managed
+  depends_on "node" => [:recommended] # Can be ignored if node is already managed
 
   conflicts_with "hadoop", :because => "both install `yarn` binaries"
   conflicts_with "yarn", :because => "yvm installs and manages yarn"
 
   def install
+    File.write("#{ENV["HOME"]}/.bashrc", "")
+    mkdir_p "#{ENV["HOME"]}/.config/fish"
+    File.write("#{ENV["HOME"]}/.config/fish/config.fish", "")
+    ENV["YVM_INSTALL_DIR"] = "."
+    system "node", "yvm.js", "configure-shell"
     update_self_disabled = "echo 'YVM update-self disabled. Use `brew upgrade yvm`.'"
     inreplace "yvm.sh" do |s|
       s.gsub! 'YVM_DIR=${YVM_DIR-"${HOME}/.yvm"}', "YVM_DIR='#{prefix}'"
@@ -23,8 +28,6 @@ class Yvm < Formula
       s.gsub! 'set -q YVM_DIR; or set -gx YVM_DIR "$HOME/.yvm"', "set -gx YVM_DIR '#{prefix}'"
       s.gsub! "env YVM_INSTALL_DIR=$YVM_DIR curl -fsSL https://raw.githubusercontent.com/tophat/yvm/master/scripts/install.js | node", update_self_disabled
     end
-    chmod 0755, "yvm.sh"
-    chmod 0755, "shim/yarn"
     mkdir_p "/usr/local/var/yvm/versions"
     ln_sf "/usr/local/var/yvm/versions", "./versions"
     File.write(".version", "{ \"version\": \"#{version}\" }")
@@ -46,7 +49,7 @@ class Yvm < Formula
   test do
     File.write("#{ENV["HOME"]}/.bashrc", "")
     ENV["YVM_INSTALL_DIR"] = prefix.to_s
-    system "#{prefix}/yvm.sh", "configure-shell"
+    system "node", "#{prefix}/yvm.js", "configure-shell"
     system "bash -i -c 'echo $YVM_DIR'"
     system "bash -i -c 'yvm ls-remote'"
     ENV["YVM_DIR"] = prefix.to_s
