8c62ee27b64ccd8d4b69108d8748bd9e0c5645b2
Emmanuel Ogbizi <iamogbz+github@gmail.com>
2020-09-19 11:23:49 -0400

    chore: upgrade yvm formula to v4.0.0

--- Formula/yvm.rb
+++ Formula/yvm.rb
@@ -3,8 +3,8 @@ class Yvm < Formula
   desc "Manage multiple versions of Yarn"
   homepage "https://yvm.js.org"
   # Should only be updated if a newer version is listed as a stable release
-  url "https://github.com/tophat/yvm/releases/download/v3.6.7/yvm.js"
-  sha256 "2edface548e2e077f99743ba0fdfb1705802b8f74fec2d499ae102bebff2690f"
+  url "https://github.com/tophat/yvm/releases/download/v4.0.0/yvm.js"
+  sha256 "63fa3e48c6ec7624e6ba74ad34924544e79796b1d2ebcd4e708c4050b017ed7f"
 
   bottle :unneeded
 
